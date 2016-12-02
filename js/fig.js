08e82c9f3aa3a60424522c6f0afc6346858b3775
Steeve Droz <steeve.droz@rpn.ch>
2016-12-02 17:36:44 +0100

    Ajout d'un CSS pour les images

--- js/fig.js
+++ js/fig.js
@@ -4,9 +4,10 @@ $(function() {
     figId++
     $(this).data('id', figId)
     let figFrame = $('<a>', {
-      "class": 'figFrame',
+      "class": `figFrame ${$(this).attr('class')}`,
       name: `fig${figId}`
     })
+    $(this).removeClass()
     $(this).wrap(figFrame)
     $(this).parent().append($('<div>', {
       "class": 'legend',
