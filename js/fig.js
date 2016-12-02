79364993af4c28de8a461f502c1b0a0e13eb19c8
Steeve Droz <steeve.droz@rpn.ch>
2016-12-02 17:04:52 +0100

    Ajout de la création d'un dépôt

--- js/fig.js
+++ js/fig.js
@@ -9,13 +9,15 @@ $(function() {
     })
     $(this).wrap(figFrame)
     $(this).parent().append($('<div>', {
+      "class": 'legend',
       html: `<b>Fig. ${figId}</b> : ${$(this).attr('alt')}`
     }))
   })
 
   $('.fig').each(function() {
-    let name = $(`img[src='${$(this).data('url')}']`).parent().attr('name')
+    let image = $(`img[src='${$(this).attr('href')}']`)
+    let name = image.parent().attr('name')
     $(this).attr('href', `#${name}`)
-    $(this).text(`(Fig. ${name.substr(3)})`)
+    $(this).text(`(${image.parent().find('.legend').text()})`)
   })
 })
